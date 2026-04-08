ab6c4edbc55a01e307d2aadffa1ef0e8de47f8d1
joanapimenta27 <jlsmpimenta27@gmail.com>
2026-04-08 02:28:14 +0100

    only inside circle bug

--- workspace/src/reactive_robot/reactive_robot/movement_node.py
+++ workspace/src/reactive_robot/reactive_robot/movement_node.py
@@ -21,58 +21,25 @@ class MovementNode(Node):
         self.get_logger().info('Movement node (Muscles) started')
 
     def command_callback(self, msg: Vector3):
-        angle_deg = msg.x
-        distance = msg.y
-        stop = msg.z
+       
 
         twist = Twist()
 
         # --- 0. STOP COMMAND ---
-        if stop == 1.0:
+        if msg.z == 1.0:
+            twist.linear.x = 0.0
+            twist.angular.z = 0.0
             self.vel_pub.publish(twist)
             self.get_logger().info('Mission Complete. Stopping.')
             return
 
-        # --- 1. TURN ---
-        # Normalize the angle: Turning 270 degrees left is the same as turning 90 degrees right.
-        if angle_deg > 180:
-            turn_angle_deg = 360.0 - angle_deg
-            angular_speed = -0.5  # Spin right (negative)
-        else:
-            turn_angle_deg = angle_deg
-            angular_speed = 0.5   # Spin left (positive)
-
-
-        self.get_logger().info(f'MUSCLES: Starting movement! Turn: {turn_angle_deg}deg, Drive: {distance}m')
+        # --- 1. CONTINUOUS MOVEMENT ---
+        twist.linear.x = float(msg.y)
+        twist.angular.z = float(msg.x)
         
-        if turn_angle_deg > 0:
-            twist.angular.z = angular_speed
-            self.vel_pub.publish(twist)
-            
-            
-            turn_time = math.radians(turn_angle_deg) / abs(angular_speed)
-            time.sleep(turn_time)
-
-        # --- 2. MOVE FORWARD ---
-        if distance > 0:
-            # Stop turning, start moving forward
-            twist.angular.z = 0.0
-            twist.linear.x = 0.2  # Set linear speed to 0.2 m/s
-            self.vel_pub.publish(twist)
-            
-            move_time = distance / 0.2
-            time.sleep(move_time)
-
-        # --- 3. STOP AND REPORT DONE ---
-        twist.linear.x = 0.0
-        twist.angular.z = 0.0
+        # Publish the speeds immediately
         self.vel_pub.publish(twist)
         
-        # Send an empty Vector3 message back to the brain to say "I'm ready for the next move"
-        done_msg = Vector3()
-        self.done_pub.publish(done_msg)
-        self.get_logger().info(f'Finished moving: turned {turn_angle_deg}deg, drove {distance}m.')
-
 def main(args=None):
     rclpy.init(args=args)
     node = MovementNode()
